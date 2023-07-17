8d888ed8532b6ed8bb6c9f514e422802b09b9af7
EC2 Default User <ec2-user@ip-172-31-23-99.ap-northeast-1.compute.internal>
2023-07-17 13:44:54 +0000

    add

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -61,8 +61,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
 #   protected
-  
-#   def configure_permitted_parameters
-#     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-#   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email])
+  end
 end
