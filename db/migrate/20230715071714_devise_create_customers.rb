747f60aae814f7e0c0885461f4d0e8719816e7a6
EC2 Default User <ec2-user@ip-172-31-23-99.ap-northeast-1.compute.internal>
2023-07-17 05:32:39 +0000

    update_karamu

--- db/migrate/20230715071714_devise_create_customers.rb
+++ db/migrate/20230715071714_devise_create_customers.rb
@@ -3,7 +3,7 @@
 class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
-      ## Database authenticatable
+      ## Database authenticatabl
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -32,7 +32,14 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :postcode, null: false
+      t.string :address, null: false
+      t.string :phone_number, null: false
+      t.boolean :is_withdrawal, null: false, default: false
       t.timestamps null: false
     end
 
--- db/migrate/20230715071956_create_sends.rb
+++ db/migrate/20230715071956_create_sends.rb
@@ -1,6 +1,11 @@
 class CreateSends < ActiveRecord::Migration[6.1]
   def change
     create_table :sends do |t|
+      t.integer :customer_id, null: false
+      t.string :name, null: false
+      t.string :postcode, null: false
+      t.string :address, null: false
+
 
       t.timestamps
     end
