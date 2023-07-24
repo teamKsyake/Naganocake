d3c0020680f9b6e675af648cd41fa4bf43dc8a2c
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-07-24 10:26:33 +0000

    [update]public-item

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,18 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.where(is_available: true)
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new(cart_item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :price)
+  end
+  def cart_item_params
+    params.require(:cart_item).permit(:costomer_id, :item_id, :amount)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,6 @@ class Item < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/about-img.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      image.variant(resize_to_limit: [100, 100]).processed
+      image
   end
 end
