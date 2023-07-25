24506d01c109d98c08631d32ddb42e0e49378d46
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-07-25 04:04:57 +0000

    [add]publicitems

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,13 +5,14 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @cart_item = CartItem.new(cart_item_params)
+    @cart_item = CartItem
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :image, :price)
   end
+  
   def cart_item_params
     params.require(:cart_item).permit(:costomer_id, :item_id, :amount)
   end
