7fc4597135be510a62d408c6213acc383304a59d
EC2 Default User <ec2-user@ip-172-31-23-99.ap-northeast-1.compute.internal>
2023-07-27 09:49:36 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,23 +2,63 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+    @address = Address.new
   end
+
+  def check
+     @order = current_customer.orders.new(order_params)
+     @order.postage = 800
+
+     @item_total_price = 0
+     current_customer.cart_items.each do |cart_item|
+       subtotal_price = cart_item.item.price * cart_item.amount * 110 / 100
+       @item_total_price += subtotal_price
+     end
+     @order.total_amount = @item_total_price + @order.postage
+
+      if params[:order][:select_address] == "0"
+        @order.postcode = current_customer.postcode
+        @order.address = current_customer.address
+        @order.name = "#{current_customer.last_name}#{current_customer.first_name}"
+      elsif params[:order][:select_address] == "1"
+        @address = Address.find(params[:address_id])
+        @order.postcode = @address.postcode
+        @order.address = @address.address
+        @order.name = @address.name
+      elsif params[:order][:select_address] == "2"
+        @address = current_customer.address.new(address_params)
+        if @address.valid?
+          @order.postcode = @address.postcode
+          @order.address = @address.address
+          @order.name = @address.name
+        else
+          render 'new'
+        end
+      end
+
+
+
+  # @order.payment = current_customer.cart_items.inject(0){|sum, cart_item| cart_item.subtotal_price + sum} + @order.postage
+end
 
   def create
     @order = current_customer.orders.build(order_params)
+    @order.postage = 800
+    @cart_items=current_customer.cart_items
     # 注文処理のロジックを追加
     if @order.save
       # 保存が成功した場合の処理
+      @cart_items.each do |cart_item|
+        ItemOrder.create!(
+          order_id:@order.id,item_id: cart_item.item_id,price:cart_item.item.with_tax_price,amount: cart_item.amount
+          )
+      end
+      current_customer.cart_items.destroy_all
       redirect_to order_path(@order)
     else
       # 保存が失敗した場合の処理
       render :new
     end
-  end
-
-  def check
-    # 注文情報確認ページ用のアクション
-    @order = Order.find(params[:id])
   end
 
   def complete_order
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,3 +1,7 @@
 class ItemOrder < ApplicationRecord
   has_one_attached :image
+  belongs_to :order
+  belongs_to :item
+  enum production_status: {"着手不可"=>0, "製作待ち"=>1, "制作中"=> 2, "製作完了"=> 3}
+
 end
